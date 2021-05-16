8e9c1348e17deb4bf89a555138b250163c55d47d
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-05-16 22:16:01 +0200

    Add dishes switch

--- src/form/Form.js
+++ src/form/Form.js
@@ -4,8 +4,18 @@ import "./Form.css";
 function Form() {
     return (
         <form className="conteiner-form">
+            <div className="food-image">
+                <img src="\img\pizza.png"/>
+            </div>
             <p className="text1">Menu<span></span></p>
             <p className="text2">Delicious<br/> Food is Waiting<br/> For you</p>
+            <div className="dishes-box">
+                <a href="#">Pizza</a>
+                <a href="#">Soup</a>
+                <a href="#">Sandwich</a>
+            </div>
+            
+            
         </form>
     )
 }
