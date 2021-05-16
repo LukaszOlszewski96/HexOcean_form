0138ad7c36c4217e1c43c4383563ebb683892bef
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-05-16 21:55:10 +0200

    First working repo, background and conteiner menu form

---
+++ src/form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Form.css";
+
+function Form() {
+    return (
+        <form className="conteiner-form">
+            <p className="text1">Menu<span></span></p>
+            <p className="text2">Delicious<br/> Food is Waiting<br/> For you</p>
+        </form>
+    )
+}
+
+export default Form;
+
