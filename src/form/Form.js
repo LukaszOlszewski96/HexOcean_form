0787dd40e8c1888dfe68f1c4ca43842f80574c00
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-05-16 22:51:36 +0200

    Add image dishes

--- src/form/Form.js
+++ src/form/Form.js
@@ -5,10 +5,16 @@ function Form() {
     return (
         <form className="conteiner-form">
             <div className="food-image">
-                <img src="\img\pizza.png"/>
+                <img src="\image\pizza.png"/>
+                <span></span>
+                <p>Salami Pizza</p>
             </div>
             <p className="text1">Menu<span></span></p>
             <p className="text2">Delicious<br/> Food is Waiting<br/> For you</p>
+            <div class="input-field">
+                <input type="text" id="name" required />
+                <label for="name">Dish name:</label>
+            </div>
             <div className="dishes-box">
                 <a href="#">Pizza</a>
                 <a href="#">Soup</a>
